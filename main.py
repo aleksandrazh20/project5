b1f418de4f2e7c1023f1819967559222a1ade485
aleksandrazh20 <hollandjoe20@gmail.com>
2020-03-14 08:56:25 +0700

    123

---
+++ main.py
@@ -0,0 +1,46 @@
+# Вариант 1, бинарный поиск
+
+import time
+
+def binary_search_rec(a,n):
+    if len(a) == 0:
+        return None
+    else:
+        mid = len(a) // 2
+    if a[mid] == n:
+        return mid
+    else:
+        if n < a[mid]:
+            return binary_search_rec(a[:mid],n)
+        else:
+            return binary_search_rec(a[mid + 1:],n)
+
+def binary_search_it(a,n):
+    mid = len(a) // 2
+    first = 0
+    last = len(a) - 1
+
+    while a[mid] != n and first <= last:
+        if n > a[mid]:
+            first = mid + 1
+        else:
+            last = mid - 1
+        mid = (first + last) // 2
+
+    if first > last:
+        return None
+    else:
+        return mid
+
+
+a = list(map(int, input().split()))
+n = int(input())
+type = input('Recursion, iteration: ').lower()
+if type == 'recursion':
+    print(binary_search_rec(a,n))
+    start = time.time()
+    print('Time: ' + str(time.time()-start) + 'sec')
+else:
+    print(binary_search_it(a,n))
+    start = time.time()
+    print('Time: ' + str(time.time() - start) + 'sec')
